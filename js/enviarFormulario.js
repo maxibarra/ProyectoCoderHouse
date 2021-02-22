db3080cde5e7f0921fd8b07131c3406a5b865f78
Maxi Ibarra <maxi.8379@gmail.com>
2021-02-22 01:29:35 -0300

    modificaciones de estilo

---
+++ js/enviarFormulario.js
@@ -0,0 +1,33 @@
+let nombre = document.getElementById('txtNombre');
+let correo = document.getElementById('txtCorreo');
+let mensaje = document.getElementById('txtMensaje');
+
+const form = document.getElementById('contacto');
+
+form.addEventListener('submit', (e) => {
+    
+    e.preventDefault(); 
+      
+      enviarFormulario(nombre.value,correo.value,mensaje.value);
+
+      form.reset();
+   })
+function enviarFormulario(nombre, correo, mensaje) {
+    Email.send({
+        SecureToken : "44fa6039-dd5f-4ea3-a709-b55dd5e461a0",
+        To: "maxi.8379@gmail.com",
+        From: "info@carpinteriamax.com",
+        Subject: `Recibiste un mensaje de la pagina web`,
+        Body: `<p>Nombre: <b>${nombre}</b></p>
+                <p>Email: <b>${correo}</b></p>
+                <p>Consulta: <b>${mensaje}</b></p>
+        `
+        }).then(
+        message=>swal("Consulta enviada correctamente", "Muchas gracias por comunicarte, nos pondremos en contacto pronto", "success")
+    )
+    .catch(error => console.log('error: ', error))
+
+    if(enviarFormulario==true){
+        console.log("Mensaje enviado")
+    }
+}
